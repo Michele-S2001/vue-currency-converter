3f8841ea7f4d20c757b16c5be09d2ffb982af78c
MicheleSerafini <serafini.michele01@gmail.com>
2024-05-08 11:43:25 +0200

    ADD > Milestone 2: setUp default selected values and install axios for currencies fetching

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios';
+import store from './store';
+import CurrencyConversionWidget from './components/CurrencyConversionWidget.vue';
+
+export default {
+  components: {
+    CurrencyConversionWidget
+  },
+
+  data() {
+    return {
+      currencies: null
+    }
+  },
+
+  methods: {
+    fetchCurrencies() {
+      axios
+        .get(`${store.BASE_API_URL}/currencies`)
+        .then(res => {
+          this.currencies = res.data;
+        })
+    }
+  },
+
+  mounted() {
+    this.fetchCurrencies();
+  }
+}
+
+</script>
+
+<template>
+  <h1>ciao</h1>
+  <CurrencyConversionWidget :currencies="currencies" :defaultSelectedValue="'EUR'"/>
+  <CurrencyConversionWidget :currencies="currencies" :defaultSelectedValue="'USD'"/>
+</template>
+
+<style>
+
+</style>
--- src/components/CurrencyConversionWidget.vue
+++ src/components/CurrencyConversionWidget.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="conversion-widget">
     <input type="text">
-    <select name="currencies">
-      <option value="#">valuta</option>
+    <select name="currencies" v-model="selectedCurrency">
+      <template v-if="currencies">
+        <option 
+          v-for="(currency, key, idx) in currencies" 
+          :key="idx"
+          :value="key"
+        >
+          {{ currency }}
+        </option>
+      </template>
+      <option v-else value="#">caricamento valute...</option>
     </select>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    currencies: {
+      type: [Object, null],
+      required: true
+    },
+    defaultSelectedValue: {
+      type: String,
+      validator: function(v) {
+        return v === 'USD' || v === 'EUR';
+      },
+      required: true
+    }
+  },
 
+  data() {
+    return {
+      selectedCurrency: this.defaultSelectedValue
+    }
+  }
 }
 
 </script>
 
-<style lang="scss" scoped>
+<style>
 
 </style>
