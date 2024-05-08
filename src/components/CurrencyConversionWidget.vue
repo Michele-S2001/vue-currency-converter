d310ccf3b53d3e27c2913b5fbdecb9bbd003d33d
MicheleSerafini <serafini.michele01@gmail.com>
2024-05-08 10:56:29 +0200

    ADD > Draft of currencyConversionWidget

---
+++ src/components/CurrencyConversionWidget.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="conversion-widget">
+    <input type="text">
+    <select name="currencies">
+      <option value="#">valuta</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
